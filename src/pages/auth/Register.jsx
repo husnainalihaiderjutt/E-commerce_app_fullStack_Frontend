42e46471a005c8c0cecca9a1ce8d834d6a343328
husnainalihaiderjutt <161064154+husnainalihaiderjutt@users.noreply.github.com>
2026-07-17 19:48:42 +0500

    Registration of user with redux toolkit

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import CommonForm from '@/components/common/Form';
 import { registerFormControls } from '@/config';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../store/auth-slice/index';
+import { toast } from "sonner"
+
 const initialState = {
   userName:'',
   email:'',
@@ -9,13 +13,28 @@ const initialState = {
 }
 const AuthRegister = () => {
   const [formData,setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const onSubmit = (event)=>
+  {
+     event.preventDefault();
+     dispatch(registerUser(formData)).then((data)=>{
+      if (data.payload?.success) {
+      toast.success("Registration successful!", { position: "top-right" });
+      navigate("/auth/login");
+      }   
+      else {
+      toast.error(data.payload?.message || "Registration failed"  , { position: "top-right" });
+      }
+     })
+  }
   return (
     <div className='mx-auto w-full max-w-md space-y-6'>
       <div className='text-center'>
       <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create New Account</h1>
       </div>
       <CommonForm formControls={registerFormControls} formData={formData} 
-      setFormData={setFormData}
+      setFormData={setFormData} onSubmit={onSubmit}
       buttonText={'Sign up'}></CommonForm>
       <div className='text-center'>
               <p className='mt-2 inline'>Already have an account</p>
